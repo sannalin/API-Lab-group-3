c3148c4b8a8a8bdea28aac18a698f8e832d78add
Sanna <sanna.ma.lindholm@gmail.com>
2021-03-11 09:59:26 +0100

    changing starting angle

--- Sanna Lindholm/Prototype/app.js
+++ Sanna Lindholm/Prototype/app.js
@@ -10,7 +10,7 @@ let servoOn = false;
 // when the arduino is connected run the function
 board.on("ready", () => {
   // set the servo to pin 10 and also set the starting position to center by default 90 degrees
-  const servo = new Servo({ pin: 10, center: true });
+  const servo = new Servo({ pin: 10, startAt: 180 });
 
   // listen to local port 3001 using the express API to read the
   app.listen(3001, () => {});
@@ -23,12 +23,12 @@ board.on("ready", () => {
   function angrySweep() {
     // the servo will sweep between 85 and 95 degrees in 75 ms after 5000ms the servo will stop and return to a position of 90 degrees
     servo.sweep({
-      range: [85, 95],
+      range: [180, 175],
       interval: 75,
     });
     setTimeout(function stop() {
       servo.stop();
-      servo.home(90);
+      servo.home(180);
       servoOn = false;
     }, 5000);
   }
@@ -37,12 +37,12 @@ board.on("ready", () => {
   // soil moisture starts to get low, needs water
   function waterSweep() {
     servo.sweep({
-      range: [83, 97],
+      range: [180, 165],
       interval: 300,
     });
     setTimeout(function stop() {
       servo.stop();
-      servo.home(90);
+      servo.home(180);
       servoOn = false;
     }, 5000);
   }
@@ -51,12 +51,12 @@ board.on("ready", () => {
   // sufficient soil moisture
   function happySweep() {
     servo.sweep({
-      range: [80, 100],
+      range: [180, 160],
       interval: 1500,
     });
     setTimeout(function stop() {
       servo.stop();
-      servo.home(90);
+      servo.home(180);
       servoOn = false;
     }, 8000);
   }
