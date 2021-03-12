3b60f5539fb06c70261f89a360d27f5f4c05c855
AnnaFromMalmo <korczak.anna.marta@gmail.com>
2021-03-12 17:55:14 +0100

    adding formatting and comments

--- Anna Korczak/Prototype/track.js
+++ Anna Korczak/Prototype/track.js
@@ -1,35 +1,37 @@
+//console log to know if everything is connected
 console.log("Ready!")
-
+//variables
 const video = document.getElementById("myvideo");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
 let updateNote = document.getElementById("updatenote");
 let plantVideo = document.getElementById("movement");
-
 let isVideo = false;
 let model = null;
-
+//setting model parameters
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video  
     maxNumBoxes: 1,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.6,    // confidence threshold for predictions.
-}
-
+    scoreThreshold: 0.6,    // confidence threshold for predictions
+};
+//function, which starts the video with detection
 function startVideo() {
-    handTrack.startVideo(video).then(function(status) {
+    handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
+        //if the video is started, detection starts
         if (status) {
             updateNote.innerText = "Video started. Now tracking"
             isVideo = true
             runDetection()
+            //otherwise, it asks to enable video
         } else {
             updateNote.innerText = "Please enable video"
         }
     });
-}
-
+};
+//button function, which allows to toggle video
 function toggleVideo() {
     if (!isVideo) {
         updateNote.innerText = "Starting video"
@@ -40,28 +42,30 @@ function toggleVideo() {
         isVideo = false;
         updateNote.innerText = "Video stopped"
     }
-}
-
+};
+//function, which shakes the plant when a hand is detected on the canvas
 function runDetection() {
     model.detect(video).then((predictions) => {
-    console.log("Predictions: ", predictions);
-    model.renderPredictions(predictions, canvas, context, video);
-    if (isVideo) {
-        requestAnimationFrame(runDetection);
-    }
-    if (predictions.length >= 1) {
-        playVideo();
-    }
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        //run detection when the video is on
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+        //start shaking the plant when a hand appears on the canvas
+        if (predictions.length >= 1) {
+            playVideo();
+        }
     });
-}
-
+};
+//function, which enables the shaking plant video to play
 function playVideo() {
     console.log("Shaky shaky")
     plantVideo.play()
-}
-
+};
+//load the handtrack.js model
 handTrack.load(modelParams).then(lmodel => {
-        // detect objects in the image.
+    // detect objects in the image.
     model = lmodel
     updateNote.innerText = "Model loaded!"
     console.log("Model loaded!");
--- Anna Korczak/example-01/script.js
+++ Anna Korczak/example-01/script.js
@@ -1,19 +1,21 @@
+//console log to know if everything is connected
+console.log("Ready!")
+//variables
 const video = document.getElementById("myvideo");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
 let updateNote = document.getElementById("updatenote");
-
 let isVideo = false;
 let model = null;
-
+//setting model parameters
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video  
     maxNumBoxes: 20,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.6,    // confidence threshold for predictions.
-}
-
+    scoreThreshold: 0.6,    // confidence threshold for predictions
+};
+//function, which starts the video with detection
 function startVideo() {
     handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
@@ -25,8 +27,8 @@ function startVideo() {
             updateNote.innerText = "Please enable video"
         }
     });
-}
-
+};
+//button function, which allows to toggle video
 function toggleVideo() {
     if (!isVideo) {
         updateNote.innerText = "Starting video"
@@ -37,10 +39,8 @@ function toggleVideo() {
         isVideo = false;
         updateNote.innerText = "Video stopped"
     }
-}
-
-
-
+};
+//function, which states condition for detecting a hand
 function runDetection() {
     model.detect(video).then(predictions => {
         console.log("Predictions: ", predictions);
@@ -49,11 +49,9 @@ function runDetection() {
             requestAnimationFrame(runDetection);
         }
     });
-}
-
-// Load the model.
+};
+//load the handtrack.js model
 handTrack.load(modelParams).then(lmodel => {
-    // detect objects in the image.
     model = lmodel
     updateNote.innerText = "Loaded Model!"
     trackButton.disabled = false
